b34afa1728802be355fad364ddffc9e27cc025a7
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-03-19 09:11:45 +0100

    Create a test for POST request

--- routes.test.js
+++ routes.test.js
@@ -1,42 +1,102 @@
 const express = require("express");
 const serverRoutes = require("./routes");
 const request = require("supertest");
-
 const app = express();
+const { save } = require("./save_json");
+const bodyParser = require("body-parser");
 
-let firstCountry;
+jest.mock("./countries.json", () => [
+    {
+      "short": "EN",
+      "name": "England",
+      "capital": "London"
+    },
+    {
+      "short": "DE",
+      "name": "Germany",
+      "capital": "Berlin"
+    },
+    {
+      "short": "PL",
+      "name": "Poland",
+      "capital": "Warsaw"
+    }
+])
+jest.mock("./save_json", () => ({
+  save: jest.fn(),
+}));
+
+app.use(bodyParser.json());
 app.use("/countries", serverRoutes);
-describe('testing-server-routes', () => {
-    test('GET /countries - success', async () => {
-      const { body } = await request(app).get('/countries');
-      expect(body).toEqual([
-        {
-          "short": "EN",
-          "name": "England",
-          "capital": "London"
-        },
-        {
-          "short": "DE",
-          "name": "Germany",
-          "capital": "Berlin"
-        },
-        {
-          "short": "PL",
-          "name": "Poland",
-          "capital": "Warsaw"
-        },
-        {
-          "short": "IT",
-          "name": "Italy",
-          "capital": "Rome"
-        }
-      ]);
-      firstCountry = body[0];
+let firstCountry;
+describe("testing-server-routes", () => {
+  test("POST /countries - success", async () => {
+    let countryObj = {
+      code: "IT",
+      name: "Italy",
+      capital: "Rome"
+    }
+    const { body } = await request(app).post("/countries").send(countryObj);
+    expect(body).toEqual({
+      status: "success",
+      countryInfo: {
+        code: "IT",
+        name: "Italy",
+        capital: "Rome"
+      },
     });
+    expect(save).toHaveBeenCalledWith([
+      {
+        short: "EN",
+        name: "England",
+        capital: "London"
+      },
+      {
+        short: "DE",
+        name: "Germany",
+        capital: "Berlin"
+      },
+      {
+        short: "PL",
+        name: "Poland",
+        capital: "Warsaw"
+      },
+      {
+        code: "IT",
+        name: "Italy",
+        capital: "Rome"
+      }
+    ]);
+  });
+  test("GET /countries - success", async () => {
+    const { body } = await request(app).get("/countries");
+    expect(body).toEqual([
+      {
+        "short": "EN",
+        "name": "England",
+        "capital": "London"
+      },
+      {
+        "short": "DE",
+        "name": "Germany",
+        "capital": "Berlin"
+      },
+      {
+        "short": "PL",
+        "name": "Poland",
+        "capital": "Warsaw"
+      },
+      {
+        "code": "IT",
+        "name": "Italy",
+        "capital": "Rome"
+      }
+    ]);
+    firstCountry = body[0];
+  });
 
-    test('GET /countries/England - success', async () => {
-      const { body } = await request(app).get(`/countries/${firstCountry.name}`);
-      expect(body).toEqual(firstCountry)
-    })
+  test("GET /countries/England - success", async () => {
+    const { body } = await request(app).get(`/countries/${firstCountry.name}`);
+    expect(body).toEqual(firstCountry);
+  });
 });
-  
